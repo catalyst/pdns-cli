1e8491e09af322b6f2ac15a0d2ac40f2a480d639
Francis Devine <francis@catalyst.net.nz>
2019-12-12 10:03:43 +1300

    bin scripts now assume you have a conf path specified

--- bin/mass-update-ttls.sh
+++ bin/mass-update-ttls.sh
@@ -52,16 +52,16 @@ done
 if [ -z "${zone}" ]; then
     error "You must supply a zone to operate on"
 fi
-domains="$(./pdns -c conf.toml show-rrsets $zone | awk '{ print $1" "$4" "$5}')"
+domains="$(./pdns show-rrsets $zone | awk '{ print $1" "$4" "$5}')"
 if [ ! -z "${filter}" ]; then
     domains="$(echo "$domains" | grep $filter)"
 fi
 #don't include non CNAME/ALIAS/A/AAAA domains 
 domains="$(echo "$domains" | grep -v "TXT" | grep -v "MX" | grep -v "SOA" | grep -v "NS")"
 while read -r line; do
-    echo "./pdns -c conf.toml edit-rrset --ttl $ttl --replace $zone $line";
+    echo "./pdns edit-rrset --ttl $ttl --replace $zone $line";
     if [ ! -z "${do}" ]; then
-        ./pdns -c conf.toml edit-rrset --ttl $ttl --replace $zone $line
+        ./pdns edit-rrset --ttl $ttl --replace $zone $line
     fi;
 done <<< "$domains"
 exit ${rc}
